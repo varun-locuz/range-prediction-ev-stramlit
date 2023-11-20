ca0c5a292954934482d338226922caff933a3706
varun-locuz <116337453+varun-locuz@users.noreply.github.com>
2023-11-20 16:56:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import streamlit as st
 pickle_a=open("random_forest_model_weights.pkl","rb")
 regressor=pickle.load(pickle_a) # our model
 
-def predict_range(time travelled,Speed,battery degraded):
-    prediction=regressor.predict([[time travelled,Speed,battery degraded]]) #predictions using our model
+def predict_range(time_travelled,Speed,battery_degraded):
+    prediction=regressor.predict([[time_travelled,Speed,battery_degraded]]) #predictions using our model
     return prediction 
 
 
@@ -24,7 +24,7 @@ def main():
     battery degraded=st.text_input("battery degraded") #giving inputs as used in building the model
     result=""
     if st.button("Predict"):
-        result=predict_chance(time travelled,Speed,battery degraded) #result will be displayed if button is pressed
+        result=predict_chance(time_travelled,Speed,battery_degraded) #result will be displayed if button is pressed
     st.success("Predicted range{}".format(result))
         
 if __name__=='__main__':
